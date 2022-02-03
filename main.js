13d4e1f3f665ede44c38d764128376f28b984740
omarreynoso09 <omarreynoso09@gmail.com>
2022-02-03 14:48:17 -0500

    added main js

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log ("helloooooo");
+
+let person = "fred";
+let person2 = "todd";
+let people  = person + " and " + person2;
+
+console.log (people);
+
+
+let x = 5;
+let y = 10;
+let sum = x+y ; 
+console.log("the sum of both number is "+ sum);
